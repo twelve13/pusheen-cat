3d715eea6f97fe1f23f0a9e363612e24c6cb8aff
Caroline Chen <carolinecaroline@gmail.com>
2017-09-16 09:25:36 -0400

    animate

---
+++ script.js
@@ -0,0 +1,28 @@
+// vanilla javascript, without the delay
+
+// const abs = document.getElementsByTagName("DIV");
+
+// const button = document.getElementById("button");
+
+// function position() {
+// 	for(var i=0; i<abs.length; i++){
+// 		abs[i].classList.add("abso");
+// 	}
+// }
+
+// button.addEventListener("click", position)
+
+const abs = $("div");
+
+const button = $("#button");
+
+function position(){
+	abs.each(function(i){
+		var abso = $(this);
+		setTimeout(function(){
+			abso.addClass("abso");
+		}, i*300)
+	})
+}
+
+button.click(position)
